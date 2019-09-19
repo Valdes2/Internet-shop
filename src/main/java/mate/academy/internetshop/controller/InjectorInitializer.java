3b7e8ef684a719d491e74ce60cb55afd7a4b2297
Valdes2 <52889938+Valdes2@users.noreply.github.com>
2019-09-19 20:22:23 +0300

    added solution for homework 17.1_Logging

--- src/main/java/mate/academy/internetshop/controller/InjectorInitializer.java
+++ src/main/java/mate/academy/internetshop/controller/InjectorInitializer.java
@@ -15,7 +15,7 @@ public class InjectorInitializer implements ServletContextListener {
             logger.debug("Injection started...");
             Injector.injectDependency();
         } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
+            logger.fatal("Injection was failed!", new RuntimeException(e));
         }
     }
 }
